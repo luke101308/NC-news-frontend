25604ba19ef492b80c5ff6a6c9ff80b225d1e44d
luke101308 <lukebirkett113@hotmail.com>
2018-07-08 15:04:47 +0100

    added additional dialogue to frontpage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Homepage(props) {
-    return <h2>Welcome to Northcoders News</h2>
+    return <React.Fragment>
+        <h2>Welcome to Northcoders News</h2>
+        <p> Please feel free to read any article that takes your fancy, but most functionality is offline until you are logged in.</p>
+        </React.Fragment>
+
 }
 
 export default Homepage
